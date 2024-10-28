8b94b0bbab3136fdd700371027c737f83f8ecfbc
Joseph Ayoade <ayoadejoe@outlook.com>
2024-10-28 19:45:55 +0000

    CandyObject
    The creation of candy object

---
+++ oopsOneProject/src/taskOne/CandyObject.java
@@ -0,0 +1,16 @@
+package taskOne;
+
+public class CandyObject {
+
+	public CandyObject() {
+		
+	}
+	
+	public CandyObject(int noOfCandies) {
+		
+	}
+	
+	public CandyObject(int[] candySet, String[] winners) {
+		
+	}
+}
--- oopsOneProject/src/taskOne/Genesis.java
+++ oopsOneProject/src/taskOne/Genesis.java
@@ -4,7 +4,7 @@ public class Genesis {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome: This is Joseph's Project");
-
+		System.out.println("Yes! Work in progress!");
 	}
 
 }
